a836dbb734da5a9a49ab7a476e4d1830d3d4a41c
pramod <pypramod54@gmail.com>
2017-02-23 13:54:54 +0530

    second commit

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -3,17 +3,17 @@ import Vapor
 let drop = Droplet()
 
  //simply print message on chrome page
-drop.get("NewWebAPI") { request in
-    return "Hello, Google Chrome!"
-}
+//drop.get("NewWebAPI") { request in
+//    return "Hello, Google Chrome!"
+//}
 
 // simply pass parameter value
-//drop.get("/name",":name") { request in
-//    if let name = request.parameters["name"]?.string {
-//        return "Hello \(name)!"
-//    }
-//    return "Error retrieving parameters."
-//}
+drop.get("/name",":name") { request in
+    if let name = request.parameters["name"]?.string {
+        return "Hello \(name)!"
+    }
+    return "Error retrieving parameters."
+}
 
 //simply load html file
 //drop.get("html") { request in
